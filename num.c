8ea9063a8c541b7059fdd616e9d7afed880b6d5c
lokesh9916 <lokeshp0409@gmail.com>
2024-05-27 14:51:52 +0530

    branch

--- num.c
+++ num.c
@@ -3,13 +3,13 @@
 int main() {
     int num1, num2, sum;
     
-    printf("Enter the first num: ");
+    printf("Enter the first number: ");
     scanf("%d", &num1);
     
-    printf("Enter the second num: ");
+    printf("Enter the second number: ");
     scanf("%d", &num2);
     
-    sum = num1 + num3;
+    sum = num1 + num2;
     
     printf("The sum of %d and %d is %d.", num1, num2, sum);
     
